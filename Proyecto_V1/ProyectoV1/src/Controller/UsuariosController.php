1a8ce832418b36753e290e7e61776f331b99829e
iLevyyy <alexrodiaz12@gmail.com>
2023-11-30 16:20:55 +0100

    V2Finished

--- Proyecto_V1/ProyectoV1/src/Controller/UsuariosController.php
+++ Proyecto_V1/ProyectoV1/src/Controller/UsuariosController.php
@@ -1,11 +1,12 @@
 <?php
 
+
 namespace App\Controller;
 
 use App\Entity\Usuarios;
-use App\Form\UsuariosForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,37 +20,94 @@ class UsuariosController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    #[Route('/usuarios', name: 'listar_usuarios')]
-    public function listarUsuarios(): Response
+    #[Route('/usuarios/listar', name: 'listar_usuarios', methods: ['GET'])]
+    public function listarUsuarios(): JsonResponse
     {
+        // Obtén todos los usuarios desde el repositorio de la entidad Usuarios.
         $usuarios = $this->entityManager->getRepository(Usuarios::class)->findAll();
 
-        return $this->render('usuarios/index.html.twig', [
-            'controller_name' => 'UsuariosController',
-            'usuarios' => $usuarios,
-        ]);
-    }
+        // Prepara un array para almacenar los datos de los usuarios.
+        $usuariosData = [];
 
-    #[Route('/usuarios/crear', name: 'crear_usuario', methods: ['GET', 'POST'])]
-    public function crearUsuario(Request $request): Response
-    {
-        $usuario = new Usuarios();
-        $form = $this->createForm(UsuariosForm::class, $usuario);
-
-        if ($request->isMethod('POST')) {
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $this->entityManager->persist($usuario);
-                $this->entityManager->flush();
-
-                return $this->redirectToRoute('listar_usuarios');
-            }
+        // Itera sobre cada usuario y obtén sus características.
+        foreach ($usuarios as $usuario) {
+            $usuariosData[] = [
+                'nombre_usuario' => $usuario->getNombreUsuario(),
+                'correo' => $usuario->getCorreo(),
+                'password' => $usuario->getPassword(),
+                // Puedes agregar más campos según sea necesario.
+            ];
         }
 
-        return $this->render('usuarios/crear.html.twig', [
-            'controller_name' => 'UsuariosController',
-            'form' => $form->createView(),
-        ]);
+        // Retorna los datos de los usuarios en formato JSON.
+        return $this->json(['usuarios' => $usuariosData], Response::HTTP_OK);
+    }
+
+
+    #[Route('/usuarios/crear', name: 'crear_usuario', methods: ['POST'])]
+    public function crearUsuario(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $usuario = new Usuarios();
+        $usuario->setNombreUsuario($data['nombre_usuario']);
+        $usuario->setPassword($data['password']);
+        $usuario->setCorreo($data['correo']);
+
+        $this->entityManager->persist($usuario);
+        $this->entityManager->flush();
+
+        return $this->json(['mensaje' => 'Usuario creado correctamente'], Response::HTTP_OK);
+    }
+
+    #[Route('/usuarios/update', name: 'actualizar_usuario', methods: ['PUT'])]
+    public function actualizarUsuario(Request $request): JsonResponse
+    {
+        // Decodifica el contenido JSON de la solicitud y lo convierte en un array asociativo.
+        $data = json_decode($request->getContent(), true);
+
+        // Verifica si la ID está presente en el JSON.
+        if (!isset($data['id'])) {
+            return $this->json(['error' => 'ID del usuario no proporcionada'], Response::HTTP_BAD_REQUEST);
+        }
+
+        // Obtén el ID del usuario y el usuario que deseas actualizar desde el repositorio.
+        $id = $data['id'];
+        $usuario = $this->entityManager->getRepository(Usuarios::class)->find($id);
+
+        // Verifica si el usuario existe.
+        if (!$usuario) {
+            return $this->json(['error' => 'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        // Actualiza los campos del usuario según los datos proporcionados.
+        $usuario->setNombreUsuario($data['nombre_usuario'] ?? $usuario->getNombreUsuario());
+        $usuario->setPassword($data['password'] ?? $usuario->getPassword());
+        $usuario->setCorreo($data['correo'] ?? $usuario->getCorreo());
+
+        // Persiste los cambios en la base de datos.
+        $this->entityManager->flush();
+
+        // Retorna una respuesta JSON indicando que el usuario se actualizó correctamente.
+        return $this->json(['mensaje' => 'Usuario actualizado correctamente'], Response::HTTP_OK);
+    }
+    
+    #[Route('/usuarios/delete/{id}', name: 'eliminar_usuario', methods: ['DELETE'])]
+    public function eliminarUsuario(int $id): JsonResponse
+    {
+        // Obtén el usuario que deseas eliminar desde el repositorio.
+        $usuario = $this->entityManager->getRepository(Usuarios::class)->find($id);
+
+        // Verifica si el usuario existe.
+        if (!$usuario) {
+            return $this->json(['error' => 'Usuario no encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        // Elimina el usuario de la base de datos.
+        $this->entityManager->remove($usuario);
+        $this->entityManager->flush();
+
+        // Retorna una respuesta JSON indicando que el usuario se eliminó correctamente.
+        return $this->json(['mensaje' => 'Usuario eliminado correctamente'], Response::HTTP_OK);
     }
 }
